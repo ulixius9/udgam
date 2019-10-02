fa9b71dfd3868cac0060cd0b9750078271e80bdc
ulixius9 <39544459+ulixius9@users.noreply.github.com>
2019-10-02 17:15:13 +0530

    Update donate.php

--- html/donate.php
+++ html/donate.php
@@ -4,7 +4,7 @@ include("../config.php");
 $sql="insert into funds (Name,email,contact,amount,method) values ('".$_POST['name']."','".$_POST['email']."',".$_POST['contact'].",".$_POST['amount'].",'".$_POST['method']."')";
 $result=mysqli_query($conn,$sql);
 if (mysqli_num_rows($result)>=1){
-    echo "]sasasfsad";
+    echo "]sasasfsadaskdjas";
 }
 else{
     echo mysqli_error($conn);
